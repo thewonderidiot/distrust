edb9ec5c7519e684e2362198633f384cc2a81316
Mike Stewart <thewonderidiot@gmail.com>
2013-09-28 15:49:01 -0700

    Initial buildiness

---
+++ Makefile
@@ -0,0 +1,3 @@
+distrust:
+	mkdir -p build
+	rustc src/main.rs -o build/$@
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#[link(name = "distrust",
+       vers = "0",
+       url  = "http://github.com/thewonderidiot/distrust")];
+#[desc = "A simple disassembler in Rust"];
+#[license = "MIT"];
+#[crate_type = "bin"];
+
+fn main() {
+    println("Hello!");
+}
